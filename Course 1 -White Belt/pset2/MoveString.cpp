79854c21a0012fd3ea67aa020733c8081e8a213a
Libby <lbvsereda@gmail.com>
2018-09-08 16:04:14 +0300

    Create MoveString.cpp

---
+++ Course 1 -White Belt/pset2/MoveString.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+void MoveStrings(vector<string>& source, vector<string>& destination) {
+    for (string s : source) {
+        destination.push_back(s);
+    }
+
+    source.clear();
+}
+
+int main() {
+
+    string line;
+    string str;
+    vector<string> source;
+    vector<string> destination;
+
+    getline(cin, line);
+
+    std::istringstream stream(line);
+    while (stream >> str) {
+        source.push_back(str);
+    }
+
+    getline(cin, line);
+
+    std::istringstream stream2(line);
+    while (stream2 >> str) {
+        destination.push_back(str);
+    }
+
+
+    MoveStrings(source, destination);
+
+    int i = 0;
+    cout << "Source after:" << endl;
+    for (string s: source) {
+        cout << "string: " << i << " " << s << endl;
+        i++;
+    }
+
+    cout << endl;
+
+    i = 0;
+    cout << "Destination after:" << endl;
+    for (string s: destination) {
+        cout << "string: " << i << " " << s << endl;
+        i++;
+    }
+
+    return 0;
+}
