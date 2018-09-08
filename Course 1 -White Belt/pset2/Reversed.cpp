d63dad1086fb906331a48456a2e97de52f8ecd6f
Libby <lbvsereda@gmail.com>
2018-09-08 16:06:21 +0300

    Update Reversed.cpp

---
+++ Course 1 -White Belt/pset2/Reversed.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<int> Reversed(const vector<int>& v) {
+    vector<int> reversedV;
+    for (int i = v.size() - 1; i >= 0; i--) {
+        reversedV.push_back(v[i]);
+    }
+
+    return reversedV;
+}
+
+int main() {
+    string line;
+    int num;
+    vector<int> numbers;
+
+    getline(cin, line);
+    istringstream stream(line);
+    while(stream >> num) {
+        numbers.push_back(num);
+    }
+
+    vector<int> reversedNumbers = Reversed(numbers);
+
+    for (int i : reversedNumbers) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
