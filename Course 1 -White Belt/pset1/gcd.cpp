062102546c431487db456a489f3072b451345f47
Libby <lbvsereda@gmail.com>
2018-09-08 16:02:44 +0300

    Create gcd.cpp

---
+++ Course 1 -White Belt/pset1/gcd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+
+    a %= b;
+
+    return gcd(b, a);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << std::endl;
+    return 0;
+}
