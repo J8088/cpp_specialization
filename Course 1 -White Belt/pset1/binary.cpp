060ca7b0dd49ba96245977acaa7e44ea02e84f9e
Libby <lbvsereda@gmail.com>
2018-09-08 16:01:08 +0300

    Create binary.cpp

---
+++ Course 1 -White Belt/pset1/binary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> binary;
+    int bit = 0;
+    int num;
+    cin >> num;
+
+    while (num > 0) {
+        bit = num % 2;
+        binary.push_back(bit);
+        num /= 2;
+    }
+
+    for (int i = binary.size() - 1; i >= 0; i--) {
+        cout << binary[i];
+    }
+
+    cout << endl;
+
+    return 0;
+}
